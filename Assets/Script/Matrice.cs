4cfee158b6e3335e7d8ad43119f301ff9b1d625f
ehp84 <ehp84@sfr.fr>
2022-12-06 15:59:01 +0100

    ui validé matrice a revoir et list des coup en cours

---
+++ Assets/Script/Matrice.cs
@@ -0,0 +1,7 @@
+
+using UnityEngine;
+
+public class Matrice : MonoBehaviour {
+    public int[,] Echequier = new int[8, 8];
+    
+}
---
+++ Assets/Script/Pièce D'échec/Abstract Script/Piece.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Script;
+using UnityEngine;
+
+public  abstract class Piece : MonoBehaviour
+{
+    public int valeurUnitée;
+    public Empire empire;
+
+}
